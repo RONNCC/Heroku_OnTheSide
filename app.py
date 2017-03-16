948890b7b87ca561e5e52623bbe6917cd62b30ca
ronncc <ronnie.ghose@gmail.com>
2017-03-16 16:19:33 +0000

    uist

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
 import os
-from flask import Flask, render_template
+from flask import Flask,request, render_template
 
 app = Flask(__name__)
+
+clientid = "fovc0rXg7VytztmT1H8s9ijij4n1CygF"
+clientsec = "GQkZfyTloydHizkSfNEFFTkaddv4d4drdtS1Sb71I8ghtJYD0_03TRXdyaKpors3"
+authcode = -1
+
+@app.route('/whereiszhuoshi',methods=['GET','POST'])
+def zhuoshi():
+	if authcode == -1:
+		return """
+			<html><head><meta http-equiv="Refresh" content="0;
+			url="https://api.moves-app.com/oauth/v1/authorize?response_type=code&client_id={}&scope=<scope>"></head></html>
+		       """.format(clientid)
+
+
+@app.route('/whereiszhuoshi_auth',methods=['GET','POST'])
+def zhuoshi_auth():
+	params = request.args
+	return 'meh'
 
 @app.route('/')
 def hello():
@@ -11,22 +29,25 @@ def hello():
 def dinosaur():
 	return render_template('dino.html')
 
+@app.route('/uist')
+def uist():
+	return render_template('uist.html')
+
 @app.route('/links')
 def links():
 	return render_template('links.html')
 
 #@app.route('/css/<path:fnc>')
-def css_static(fnc):
+def css_static(fn):
 	return send_from_directory('/css/',fnc)
 
 #@app.route('/static/<path:fns>')
-def static(fns):
+def static(fn):
 	return send_from_directory('/static/',fns)
 
 #@app.route('/js/<path:fnj>')
-def js_static(fnj):
+def js_static(fn):
 	return send_from_directory(app.config['JS_STATIC'],fnj)
-
 
 
 @app.route('/temp/<name>')
@@ -37,4 +58,3 @@ if __name__ == '__main__':
 	# Bind to PORT if defined, otherwise default to 5000
 	port = int(os.environ.get('PORT',5000))
 	app.run(host='0.0.0.0',port=port,debug=True)
-
