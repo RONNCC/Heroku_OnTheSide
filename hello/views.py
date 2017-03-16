a397f13c2c000d95f223e03adb6f8c67aedf9431
RONNCC <ronnie.ghose@gmail.com>
2017-03-16 16:45:08 +0000

    renaming + gitignore

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 from .models import Greeting
 
-def index(request):
+def uist(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'uist.html')
 
